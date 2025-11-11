9a1464e61b2b185d3c6410b110ecbeb716687842
qadiya786 <qadiyanoor56@gmail.com>
2025-11-11 15:29:38 +0530

    Create evenoddandpositivenegativecombine.py

---
+++ evenoddandpositivenegativecombine.py
@@ -0,0 +1,9 @@
+n = int(input("enter the number: "))
+if n % 2 == 0 and n>0:
+  print("even and positive")
+elif n % 2 == 0 and n<0:
+  print("even and negative")
+elif n % 2 != 0 and n>o:
+  print("odd and positive")
+else:
+  print("odd and negative")
