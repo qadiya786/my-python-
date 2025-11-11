d9375260fdf508bff735d35073462926d9a2c4f5
qadiya786 <qadiyanoor56@gmail.com>
2025-11-11 15:22:45 +0530

    Create largestofthreenumbers.py

---
+++ largestofthreenumbers.py
@@ -0,0 +1,9 @@
+a = int(input("enter the first number"))
+b = int(input("enter the second number"))
+c = int(input("enter the third number"))
+if a>b:
+  print("largest")
+elif a<b:
+  print("second largest")
+else:
+  print("third largest")
